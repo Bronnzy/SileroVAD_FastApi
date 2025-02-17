944913ef0665e4107a46f837dd35ea16884cd697
Omonjon Yokubjonov <yomonjon4132@gmail.com>
2025-02-17 16:05:59 +0500

    removed comments

--- vad_check_api.py
+++ vad_check_api.py
@@ -8,12 +8,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, PositiveFloat
 import io
 
-# Initialize the FastAPI application
 app = FastAPI()
 
 origins = ["*"]
 
-# Configure CORS settings to allow requests from any origin
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -22,19 +20,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Load the Silero VAD model
 model = load_silero_vad()
 
-# Define a Pydantic model for the timestamp data
 class TimeStamps(BaseModel):
-    start: PositiveFloat = 0    # Start time of speech segment in seconds
-    end: PositiveFloat = 0    # End time of speech segment in seconds
+    start: PositiveFloat = 0
+    end: PositiveFloat = 0
 
 
-# Define a Pydantic model for the API response
 class Response(BaseModel):
-    has_voice: bool    # Indicates if speech was detected
-    speech_timestamps: list[TimeStamps] = [TimeStamps()]    # List of detected speech segments
+    has_voice: bool
+    speech_timestamps: list[TimeStamps] = [TimeStamps()]
 
 
 @app.post("/vad_check/")
@@ -48,13 +43,13 @@ async def vad_check(request: Request) -> Response:
     Returns:
     - Response: A JSON object indicating if speech was detected and the timestamps of speech segments.
     """
-    buffer = await request.body()    # Retrieve the raw audio data from the request bod
-    file_like_object = io.BytesIO(buffer)    # Convert the raw data to a file-like object
+    buffer = await request.body()
+    file_like_object = io.BytesIO(buffer)
     audio_path = file_like_object    
-    wav = read_audio(audio_path)    # Read the audio data using the read_audio function
-    speech_timestamps = get_speech_timestamps(wav, model, return_seconds=True)    # Detect speech segments
-    has_voice = bool(speech_timestamps)    # Determine if any speech was detected
-    response = Response(has_voice=has_voice, speech_timestamps=speech_timestamps)    # Create response object
+    wav = read_audio(audio_path)
+    speech_timestamps = get_speech_timestamps(wav, model, return_seconds=True)
+    has_voice = bool(speech_timestamps)
+    response = Response(has_voice=has_voice, speech_timestamps=speech_timestamps)
     return response
 
 
@@ -69,10 +64,10 @@ async def vad_check_file(file: UploadFile) -> Response:
     Returns:
     - Response: A JSON object indicating if speech was detected and the timestamps of speech segments.
     """
-    model = load_silero_vad()    # Load the Silero VAD model (reloading in each request for consistency)
-    audio_path = file.file    # Get the file object from the UploadFile
-    wav = read_audio(audio_path)    # Read the audio data using the read_audio function
-    speech_timestamps = get_speech_timestamps(wav, model, return_seconds=True)    # Detect speech segments
-    has_voice = bool(speech_timestamps)    # Determine if any speech was detected
-    response = Response(has_voice=has_voice, speech_timestamps=speech_timestamps)    # Create response object
+    model = load_silero_vad()
+    audio_path = file.file
+    wav = read_audio(audio_path)
+    speech_timestamps = get_speech_timestamps(wav, model, return_seconds=True)
+    has_voice = bool(speech_timestamps)
+    response = Response(has_voice=has_voice, speech_timestamps=speech_timestamps)
     return response
